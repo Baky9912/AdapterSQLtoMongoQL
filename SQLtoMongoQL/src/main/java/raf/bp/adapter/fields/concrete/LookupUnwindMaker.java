656f5e02cad376e084d336c58420f3d88f22108f
Marko Kocic <markokocic2000@gmail.com>
2023-06-06 19:50:35 +0200

    Fixed LookupUnwindMaker to account for more then 1 table join.

--- SQLtoMongoQL/src/main/java/raf/bp/adapter/fields/concrete/LookupUnwindMaker.java
+++ SQLtoMongoQL/src/main/java/raf/bp/adapter/fields/concrete/LookupUnwindMaker.java
@@ -10,6 +10,7 @@ import raf.bp.model.convertableSQL.from.CSQLFromTable;
 import raf.bp.sqlextractor.concrete.FromExtractor;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LookupUnwindMaker extends MongoQLMaker {
     @Override
@@ -26,11 +27,21 @@ public class LookupUnwindMaker extends MongoQLMaker {
 
         CSQLFromInfo fromTable = extractor.extractFromInfo();
 
+        int currentJoinedTableIndex = 0;
+        List<CSQLFromTable> joinedTables = fromTable.getJoinedTables();
         for (CSQLFromTable table : fromTable.getJoinedTables()) {
-            lookup = Aggregates.lookup(table.getTableName(), table.getLocalFieldName(), table.getForeignFieldName(), table.getAlias());
+            currentJoinedTableIndex++;
+
+            if (currentJoinedTableIndex > 1) {
+                String fullLocalFieldName = joinedTables.get(currentJoinedTableIndex - 2).getAlias() + "." + table.getLocalFieldName();
+                lookup = Aggregates.lookup(table.getTableName(), fullLocalFieldName, table.getForeignFieldName(), table.getAlias());
+            }
+            else
+                lookup = Aggregates.lookup(table.getTableName(), table.getLocalFieldName(), table.getForeignFieldName(), table.getAlias());
             unwind = Aggregates.unwind("$" + table.getAlias());
             result.add(lookup);
             result.add(unwind);
+
         }
 
         return result;
